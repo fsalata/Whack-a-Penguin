9cdf03207cf3989dc855e7ad98c7477f09cb28ab
Fábio Salata <s2it_fsalata@uolinc.com>
2019-10-07 15:22:32 -0300

    Setup project

---
+++ Whack-a-Penguin/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Whack-a-Penguin
+//
+//  Created by Fábio Salata on 07/10/19.
+//  Copyright © 2019 Fábio Salata. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+       
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
